9ad8ac18624280b7a7161937d09250eddcab6466
Imamsaa <imamsafii2222022@gmail.com>
2026-02-20 01:13:19 +0700

    feat : Create JWT Register

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,16 @@ use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CommentsController;
 use App\Http\Controllers\LikesController;
 use App\Http\Controllers\MessagesController;
+use App\Http\Controllers\JWTAuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function(){
+
+    Route::post('/register', [JWTAuthController::class, 'register']);
+
     //menghandle route untuk posts
     Route::prefix('posts')->group(function() {
         Route::get('/', [PostsController::class, 'index']); //Mengambil semua data post.
