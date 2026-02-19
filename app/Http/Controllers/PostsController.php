0020fcf815c3d1db4b3ff4c4d0fb5d9ed0f98125
Imamsaa <imamsafii2222022@gmail.com>
2026-02-19 15:39:11 +0700

    Create API Create Post

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Validator;
 
 class PostsController extends Controller
 {
@@ -19,5 +20,33 @@ class PostsController extends Controller
             'success' => true,
             'data' => $posts
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'content' => 'required',
+            'image_url' => 'required'
+        ]);
+
+        if ($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $post = Post::create([
+            'user_id' => $request->user_id,
+            'content' => $request->content,
+            'image_url' => $request->image_url
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Post created successfully',
+            'data' => $post
+        ], 201);
     }
 }
