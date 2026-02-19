32aec2cb54391f1fde668d9c3b6b8a073a8d18ce
Imamsaa <imamsafii2222022@gmail.com>
2026-02-20 01:42:10 +0700

    feat : Implementasi token jwt

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CommentsController;
 use App\Http\Controllers\LikesController;
 use App\Http\Controllers\MessagesController;
 use App\Http\Controllers\JWTAuthController;
+use App\Http\Middleware\JWTMiddleware;
 
 Route::prefix('v1')->group(function(){
 
@@ -14,7 +15,7 @@ Route::prefix('v1')->group(function(){
     Route::post('/login', [JWTAuthController::class, 'login']);
 
     //menghandle route untuk posts
-    Route::prefix('posts')->group(function() {
+    Route::middleware(JWTMiddleware::class)->prefix('posts')->group(function() {
         Route::get('/', [PostsController::class, 'index']); //Mengambil semua data post.
         Route::post('/', [PostsController::class, 'store']); //Menyimpan data post baru.
         Route::get('/{id}', [PostsController::class, 'show']); //Mengambil data post berdasarkan ID.
@@ -22,17 +23,17 @@ Route::prefix('v1')->group(function(){
         Route::delete('/{id}', [PostsController::class, 'destroy']); //Menghapus data post berdasarkan ID.
     });
 
-    Route::prefix('comments')->group(function(){
+    Route::middleware(JWTMiddleware::class)->prefix('comments')->group(function(){
         Route::post('/', [CommentsController::class, 'store']);
         Route::delete('/{id}', [CommentsController::class, 'destroy']);
     });
 
-    Route::prefix('likes')->group(function(){
+    Route::middleware(JWTMiddleware::class)->prefix('likes')->group(function(){
         Route::post('/', [LikesController::class, 'store']);
         Route::delete('/{id}', [LikesController::class, 'destroy']);
     });
 
-    Route::prefix('messages')->group(function(){
+    Route::middleware(JWTMiddleware::class)->prefix('messages')->group(function(){
         Route::post('/', [MessagesController::class, 'store']);
         Route::get('/{id}', [MessagesController::class, 'show']);
         Route::get('/getmessages/{id}', [MessagesController::class, 'getMessages']);
